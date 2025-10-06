ca74b45e4be86c1ebcd7e59dd8979165395d059b
CharlyMaye <charlymayeux@hotmail.fr>
2025-10-06 17:51:56 +0000

    Refactor FileTraceHandler to use a non-blocking sender for logging; remove unused file_path field and related code.

--- traces/src/trace/file_trace_handlers.rs
+++ traces/src/trace/file_trace_handlers.rs
@@ -1,48 +1,51 @@
 use crate::trace::{handlers::TraceHandler, Trace};
 use std::fs::File;
-use std::io::{prelude::*, SeekFrom};
+use std::io::prelude::*;
 use std::path::Path;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
-// TODO - ajouter un crate pour gérer des fichiers
 pub struct FileTraceHanlder {
-    file_path: String
+    sender: Sender<String>,
 }
+
 impl FileTraceHanlder {
     pub fn new(file_path: &str) -> Result<Self, std::io::Error> {
-        // Est ce que le fichier existe?
         let doesnot_exist = File::open(file_path).is_err();
         if doesnot_exist {
-            // TODO - Manage error !
             File::create_new(file_path)?;
         }
-        // Si non, on crée le fichier
-        Ok(Self {
-            file_path: file_path.to_string()
-        })
+        
+        let (sender, receiver) = channel::<String>();
+        let file_path = file_path.to_string();
+        
+        // Thread dédié pour l'écriture
+        thread::spawn(move || {
+            let path = Path::new(&file_path);
+            let mut file = File::options()
+                .write(true)
+                .append(true)
+                .open(&path)
+                .expect("Failed to open log file");
+            
+            while let Ok(message) = receiver.recv() {
+                if let Err(e) = file.write_all(message.as_bytes()) {
+                    eprintln!("Failed to write log: {}", e);
+                }
+                let _ = file.flush();
+            }
+        });
+        
+        Ok(Self { sender })
     }
 }
+
 impl Trace for FileTraceHanlder {
     fn log(&self, level: super::TraceLevel, message: &str) -> () {
         let message = format!("{} - {}\n", level, message);
-        let path = Path::new(self.file_path.as_str());
-        let display = path.display();
-        // TODO - Manage error !
-        // TODO - manage in async/thread 
-        let mut file = match File::options()
-            .write(true)
-            .append(true)
-            .open(&path) {
-            Err(why) => panic!("couldn't create {}: {}", display, why),
-            Ok(file) => file,
-        };
-        file.seek(SeekFrom::End(0)).unwrap();
-
-        // TODO - Manage error !
-        match file.write_all(message.as_str().as_bytes()){
-            Err(why) => panic!("couldn't write to {}: {}", display, why),
-            Ok(_) => (),
-        };
+        // Envoi non-bloquant vers le thread d'écriture
+        let _ = self.sender.send(message);
     }
 }
-impl TraceHandler for FileTraceHanlder {
-}
+
+impl TraceHandler for FileTraceHanlder {}
