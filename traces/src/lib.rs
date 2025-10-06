0cad259ad4c96f09e6ef4630451523e99c5d90df
CharlyMaye <charlymayeux@hotmail.fr>
2025-10-06 02:11:07 +0000

    Implement tracing module with Trace and TraceLevel enums, and logging functionality

---
+++ traces/src/lib.rs
@@ -0,0 +1,47 @@
+
+pub mod trace {
+    use std::{fmt::Display};
+    #[derive(Debug)]
+    pub enum TraceLevel {
+        Verbose,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Critical,
+        None,
+    }
+    impl Display for TraceLevel {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let level_str = match self {
+                TraceLevel::Verbose => "VERBOSE",
+                TraceLevel::Debug => "DEBUG",
+                TraceLevel::Info => "INFO",
+                TraceLevel::Warning => "WARNING",
+                TraceLevel::Error => "ERROR",
+                TraceLevel::Critical => "CRITICAL",
+                TraceLevel::None => "NONE",
+            };
+            write!(f, "[{}]", level_str)
+        }
+    }
+    
+    pub trait Trace {
+        fn log(&self, level: TraceLevel, message: &str) -> ();
+    }
+    
+    pub struct ConcreteTrace{}
+    
+    impl ConcreteTrace {
+        pub fn new() -> Self {
+            Self {}
+        }
+    }
+    impl Trace for ConcreteTrace {
+        fn log(&self, level: TraceLevel, message: &str) {
+            let message = format!("{} - {}", level, message);
+            println!("{message}");
+        }
+    }
+   
+}
--- traces/src/main.rs
+++ traces/src/main.rs
@@ -1,47 +1,4 @@
-use std::{fmt::Display};
-
-#[derive(Debug)]
-pub enum TraceLevel {
-    Verbose,
-    Debug,
-    Info,
-    Warning,
-    Error,
-    Critical,
-    None,
-}
-impl Display for TraceLevel {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let level_str = match self {
-            TraceLevel::Verbose => "VERBOSE",
-            TraceLevel::Debug => "DEBUG",
-            TraceLevel::Info => "INFO",
-            TraceLevel::Warning => "WARNING",
-            TraceLevel::Error => "ERROR",
-            TraceLevel::Critical => "CRITICAL",
-            TraceLevel::None => "NONE",
-        };
-        write!(f, "[{}]", level_str)
-    }
-}
-
-pub trait Trace {
-    fn log(&self, level: TraceLevel, message: &str) -> ();
-}
-
-struct ConcreteTrace{}
-
-impl ConcreteTrace {
-    fn new() -> Self {
-        Self {}
-    }
-}
-impl Trace for ConcreteTrace {
-    fn log(&self, level: TraceLevel, message: &str) {
-        let message = format!("{} - {}", level, message);
-        println!("{message}");
-    }
-}
+use traces::trace::{Trace, TraceLevel, ConcreteTrace};
 
 
 fn test<T: Trace>(trace: T) {
@@ -51,4 +8,13 @@ fn main() {
     let trace = ConcreteTrace::new();
     
     test(trace);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_log() {
+    }
 }
